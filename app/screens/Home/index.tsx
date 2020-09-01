5f898cb92f8d5c41624ad23a90ac5edad78961df
Rehan Chanaka <rehang@99x.lk>
2020-09-01 19:44:54 +0530

    added icon pack

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 import { useDispatch } from 'react-redux';
 
+import Icon from 'app/components/Icon'
+
 const Home: React.FC = () => {
     const dispatch = useDispatch();
 
     return (
         <>
+            <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+                <Icon name="chatbot-icon" size={40} />
+            </View>
         </>
     )
 }
