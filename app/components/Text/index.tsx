d31d666a1f428b279d10efb9e48018cd070dc0a9
Rehan Chanaka <rehang@99x.lk>
2020-09-01 19:03:44 +0530

    Added custom text

---
+++ app/components/Text/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const CustomText: React.FC = (props) => {
+    return <Text {...props} />
+}
+
+export default CustomText
---
+++ app/components/TextInput/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TextInput } from 'react-native';
+
+const CustomTextInput: React.FC = (props) => {
+    return <TextInput {...props} />
+}
+
+export default CustomTextInput
